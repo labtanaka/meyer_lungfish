644873c96901a64a47b7a993fe75dae4f62e42a3
Sergej Nowoshilow <Sergej.Nowoshilow@imp.ac.at>
2021-01-22 09:40:25 +0100

    Added some data required to run the scripts

--- scripts/calculateCoverage.py
+++ scripts/calculateCoverage.py
@@ -7,6 +7,7 @@ import array
 
 strPAF_in = sys.argv[1]
 strChr = sys.argv[2]
+nMinFragLen = int(sys.argv[3]) if len(sys.argv) > 2 else 0
 
 cov_data = None
 nLines = 0
@@ -19,7 +20,7 @@ with(open(strPAF_in, 'r')) as hFile:
         if nLines % 10_000_000 == 0:
             print(f'  Read {nLines} lines')
 
-        if targetID == strChr:
+        if targetID == strChr and int(readEnd) - int(readStart) >= nMinFragLen:
             if not cov_data:
                 cov_data = [0] * int(targetLen)
             for i in range(int(targetStart), int(targetEnd)):
@@ -31,5 +32,5 @@ sd = stats.stdev(cov_data)
 print(f'Mean coverage: {mn}')
 print(f'STD dev:       {sd}')
 
-with(open(f'{strChr}.coverage.pkl', 'wb')) as hOut:
+with(open(f'{strChr}.coverage.min{nMinFragLen}.pkl', 'wb')) as hOut:
     pickle.dump(cov_data, hOut)
